1254b35fe9743b5bd7203d380debb99cc9f47c59
Taiwoluke <taiwoluke@gmail.com>
2024-04-12 06:03:22 -0700

    Javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-let line1 = 'C is fun';
-let line2 = 'Python is cool';
-let line3 = 'JavaScript is amazing';
-console.log(line1);
-console.log(line2);
-console.log(line3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
