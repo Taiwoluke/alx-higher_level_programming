e683f3996ad3bbc17b84e1d6c2e581a1cf2ea2c7
Taiwoluke <taiwoluke@gmail.com>
2024-04-12 09:18:13 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,5 +1,5 @@
-#!/usr/bin/node
-class Rectangle {
+#kk!/usr/bin/node
+cl      102-concat.js  1-rectangle.js   3-rectangle.js  5-square.js     7-occurrences.js  9-logme.jsss Rectangle {
 }
 
 module.exports = Rectangle;
